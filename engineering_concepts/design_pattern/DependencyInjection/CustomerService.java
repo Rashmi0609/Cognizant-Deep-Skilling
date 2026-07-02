ab381827ccddbc9d2dd4127ea92e76457a879370
Rashmi0609 <rashmi63867@gmail.com>
2026-07-02 22:23:25 +0530

    Implemented Dependency Injection example

---
+++ engineering_concepts/design_pattern/DependencyInjection/CustomerService.java
@@ -0,0 +1,13 @@
+public class CustomerService {
+
+    private CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public void getCustomer(int id) {
+        System.out.println(customerRepository.findCustomerById(id));
+    }
+
+}
