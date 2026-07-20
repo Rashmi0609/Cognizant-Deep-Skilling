e1863af8e46863ee7e90a3a6e5034a387dc37bc3
Rashmi0609 <rashmi63867@gmail.com>
2026-07-20 20:24:49 +0530

    Added JUnit Exercise 1 and Exercise 3

---
+++ JUnit_Mockito/JUnit_Basic_Testing/src/main/java/com/cognizant/junit/App.java
@@ -0,0 +1,11 @@
+package com.cognizant.junit;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("JUnit Basic Testing");
+
+    }
+
+} 
---
+++ JUnit_Mockito/JUnit_Basic_Testing/src/test/java/com/cognizant/junit/AppTest.java
@@ -0,0 +1,16 @@
+package com.cognizant.junit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+
+        assertTrue(true);
+
+    }
+
+}
