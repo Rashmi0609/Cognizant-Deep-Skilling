b92c862feb9d54bd7e733553c3e12ec1c69d991b
Rashmi0609 <rashmi63867@gmail.com>
2026-07-20 20:34:34 +0530

    Added JUnit Exercise 4 - AAA Pattern, Setup and Teardown

---
+++ JUnit_Mockito/JUnit_Basic_Testing/src/test/java/com/cognizant/junit/AAATest.java
@@ -0,0 +1,36 @@
+package com.cognizant.junit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AAATest {
+
+    private int number;
+
+    @Before
+    public void setUp() {
+        System.out.println("Setup method executed");
+        number = 10;
+    }
+
+    @Test
+    public void testAddition() {
+
+        // Arrange
+        int value = number;
+
+        // Act
+        int result = value + 5;
+
+        // Assert
+        assertEquals(15, result);
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Teardown method executed");
+    }
+} 
