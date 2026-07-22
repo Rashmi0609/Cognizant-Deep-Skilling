b2252392827a3260222ca10c1d4e2cf4699b382b
Rashmi0609 <rashmi63867@gmail.com>
2026-07-22 19:55:34 +0530

    Completed Spring REST Hands-on

---
+++ SpringREST_Boot3/spring-learn/src/main/java/com/cognizant/spring_learn/service/CountryService.java
@@ -0,0 +1,26 @@
+package com.cognizant.spring_learn.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.cognizant.spring_learn.model.Country;
+
+@Service
+public class CountryService {
+
+    public Country getCountry(String code) {
+
+        List<Country> countries = new ArrayList<>();
+
+        countries.add(new Country("IN", "India"));
+        countries.add(new Country("US", "United States"));
+        countries.add(new Country("JP", "Japan"));
+
+        return countries.stream()
+                .filter(country -> country.getCode().equalsIgnoreCase(code))
+                .findFirst()
+                .orElse(null);
+    }
+} 
